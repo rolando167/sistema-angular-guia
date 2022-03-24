77f177e5ebf7a70936c6df690fe79f6cf6c5b065
Rolando HR <rolando167@hotmail.com>
2022-03-24 15:06:34 -0500

    ca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: 'home',
+    children: [
+      {path: '', loadChildren: () => import('../app/modules/home/inicio/inicio.module').then(m => m.InicioModule)},
+      {path: 'about', loadChildren: () => import('../app/modules/home/about/about.module').then(m => m.AboutModule)}
+    ]
+  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
