76dc3cae9aa16f63809ad97720058bea84cbb8a1
Rolando HR <rolando167@hotmail.com>
2022-03-24 17:25:09 -0500

    ca

--- src/app/modules/home/inicio/inicio-routing.module.ts
+++ src/app/modules/home/inicio/inicio-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlertasComponent } from './alertas/alertas.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { EventosComponent } from './eventos/eventos.component';
 import { InicioComponent } from './inicio.component';
+import { NoticiasComponent } from './noticias/noticias.component';
 
 const routes: Routes = [
   {
       path     : '',
-      component: InicioComponent
-  }
+      component: InicioComponent, // router-outlet en el HTML
+      children: [
+        { path: 'noticias', component: NoticiasComponent },
+        { path: 'equipo', component: EquipoComponent },
+        { path: 'eventos', component: EventosComponent },
+        { path: 'alertas', component: AlertasComponent }
+      ]
+  },
 ];
 
 @NgModule({
--- src/app/modules/home/inicio/inicio.module.ts
+++ src/app/modules/home/inicio/inicio.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
 import { InicioComponent } from './inicio.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { AlertasComponent } from './alertas/alertas.component';
 
 
 @NgModule({
   declarations: [
-    InicioComponent
+    InicioComponent,
+    NoticiasComponent,
+    EquipoComponent,
+    EventosComponent,
+    AlertasComponent
   ],
   imports: [
     CommonModule,
