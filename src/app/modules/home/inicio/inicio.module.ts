b734ea5ef8101f22c604949f9286d2a09299c9b5
Rolando HR <rolando167@hotmail.com>
2022-03-24 15:38:54 -0500

    funciona rutas Home

--- src/app/modules/home/inicio/inicio.module.ts
+++ src/app/modules/home/inicio/inicio.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
+import { InicioComponent } from './inicio.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InicioComponent
+  ],
   imports: [
     CommonModule,
     InicioRoutingModule
